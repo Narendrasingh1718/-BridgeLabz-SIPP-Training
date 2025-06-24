69156e065d48a104523bbe6f7bbe6a662abf2490
Narendra singh <narendrasingh21208@gmail.com>
2025-06-24 09:17:13 +0530

    [Narendra Singh] Add.a Program of Date Formatting

--- string/Build-in-function/DateFormatting.java
+++ string/Build-in-function/DateFormatting.java
@@ -4,7 +4,6 @@ import java.time.format.DateTimeFormatter;
 public class DateFormatting {
     public static void main(String[] args) {
         LocalDate currentDate = LocalDate.now();
-
         DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         DateTimeFormatter format2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         DateTimeFormatter format3 = DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy");
