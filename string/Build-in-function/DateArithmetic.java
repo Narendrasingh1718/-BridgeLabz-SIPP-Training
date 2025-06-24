3cbc6b1b147e6b27f44cf006e9d80794d028dbf8
Narendra singh <narendrasingh21208@gmail.com>
2025-06-24 09:16:14 +0530

    [Narendra Singh] Add.a Program of Date Arithmetic

--- string/Build-in-function/DateArithmetic.java
+++ string/Build-in-function/DateArithmetic.java
@@ -5,7 +5,6 @@ public class DateArithmetic {
         LocalDate inputDate = LocalDate.of(2023, 6, 19); // Example date
 
         LocalDate resultDate = inputDate.plusDays(7).plusMonths(1).plusYears(2).minusWeeks(3);
-
         System.out.println("Original Date: " + inputDate);
         System.out.println("Final Date after operations: " + resultDate);
     }
