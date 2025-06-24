abf05ee2449adb32978419e08b04fb763e4b93eb
Narendra singh <narendrasingh21208@gmail.com>
2025-06-24 09:19:04 +0530

    [Narendra Singh] Add.a maximum of the three numbers.

--- string/Build-in-function/MaxOfThreeNumbers.java
+++ string/Build-in-function/MaxOfThreeNumbers.java
@@ -9,7 +9,6 @@ public class MaxOfThreeNumbers {
     public static int findMaximum(int a, int b, int c) {
         return Math.max(a, Math.max(b, c));
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num1 = getInput("Enter first number", sc);
