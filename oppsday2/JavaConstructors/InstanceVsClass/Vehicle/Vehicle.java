f156cdc55dbab1c7a3797eae5f2a39f2bb4d5e72
Narendra singh <narendrasingh21208@gmail.com>
2025-06-26 09:40:55 +0530

    [Narendra Singh] Add.a program to Create a Vehicle class to manage the details of vehicles: Instance Variables: ownerName, vehicleType. Class Variable: registrationFee (fixed for all vehicles). Methods: An instance method displayVehicleDetails() to display owner and vehicle details. A class method updateRegistrationFee() to change the registration fee.

--- oppsday2/JavaConstructors/InstanceVsClass/Vehicle/Vehicle.java
+++ oppsday2/JavaConstructors/InstanceVsClass/Vehicle/Vehicle.java
@@ -3,7 +3,6 @@ package JavaConstructors.InstanceVsClass.Vehicle;
 public class Vehicle {
     private String ownerName;
     private String vehicleType;
-
     private static double registrationFee = 5000.0;
 
     public Vehicle(String ownerName, String vehicleType) {
